ee1a2494035e98192ac5bcd7793541fc182e181b
mowfax <mowfax@gmx.de>
2017-05-29 03:06:13 +0200

    semicolon correction

--- examples/examples.sh
+++ examples/examples.sh
@@ -4,13 +4,11 @@ while true; do
 	read -p "Do you want to run a single or multiple webcontrol instances? [s/m] " sm
 	case $sm in
 		[Ss]* ) curl -o /srv/example-single.tar.gz https://raw.githubusercontent.com/mowfax/webControl_docker/master/examples/example-single.tar.gz;
-            tar -xvf /srv/example-single.tar.gz -C /srv/;
-            break;
-            ;
-		[Mm]* )curl -o /srv/example-multi.tar.gz https://raw.githubusercontent.com/mowfax/webControl_docker/master/examples/example-multi.tar.gz;
-            tar -xvf /srv/example-multi.tar.gz -C /srv/;
-            break;
-            ;
-		* ) echo "Please answer single[S] or multiple[M].";;
+                        tar -xvf /srv/example-single.tar.gz -C /srv/;
+                        break;;
+		[Mm]* ) curl -o /srv/example-multi.tar.gz https://raw.githubusercontent.com/mowfax/webControl_docker/master/examples/example-multi.tar.gz;
+                        tar -xvf /srv/example-multi.tar.gz -C /srv/;
+                        break;;
+		* )     echo "Please answer single[S] or multiple[M].";;
 	esac
 done
