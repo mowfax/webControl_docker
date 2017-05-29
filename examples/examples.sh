66f32d882ea99de75708f6f35a99996d15702f6c
mowfax <mowfax@gmx.de>
2017-05-29 02:38:42 +0200

    correct url

--- examples/examples.sh
+++ examples/examples.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -o /srv/examples.tar.gz https://github.com/mowfax/webControl_docker/raw/master/examples/examples.tar.gz
+curl -o /srv/examples.tar.gz https://raw.githubusercontent.com/mowfax/webControl_docker/master/examples/examples.tar.gz
 
 tar -xvf /srv/examples.tar.gz -C /srv/
