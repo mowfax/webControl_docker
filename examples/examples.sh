586603b9020fa4b4146483787ebfd7b8855eef65
mowfax <mowfax@gmx.de>
2017-05-29 02:35:47 +0200

    install examples

--- examples/examples.sh
+++ examples/examples.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-curl -o /srv/examples.sh 
+curl -o /srv/examples.tar.gz https://github.com/mowfax/webControl_docker/raw/master/examples/examples.tar.gz
+
+tar -xvf /srv/examples.tar.gz -C /srv/
