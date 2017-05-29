351b21f7de03218d102a6b7c74a79722031e9ba2
mowfax <mowfax@gmx.de>
2017-05-29 03:03:25 +0200

    Ask for single or multi instance usage

--- examples/examples.sh
+++ examples/examples.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-curl -o /srv/examples.tar.gz https://raw.githubusercontent.com/mowfax/webControl_docker/master/examples/examples.tar.gz
-
-tar -xvf /srv/examples.tar.gz -C /srv/
+while true; do
+	read -p "Do you want to run a single or multiple webcontrol instances? [s/m] " sm
+	case $sm in
+		[Ss]* ) curl -o /srv/example-single.tar.gz https://raw.githubusercontent.com/mowfax/webControl_docker/master/examples/example-single.tar.gz;
+            tar -xvf /srv/example-single.tar.gz -C /srv/;
+            break;
+            ;
+		[Mm]* )curl -o /srv/example-multi.tar.gz https://raw.githubusercontent.com/mowfax/webControl_docker/master/examples/example-multi.tar.gz;
+            tar -xvf /srv/example-multi.tar.gz -C /srv/;
+            break;
+            ;
+		* ) echo "Please answer single[S] or multiple[M].";;
+	esac
+done
